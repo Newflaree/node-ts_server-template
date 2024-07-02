9fcd0abe922c94cdae56619cc7cd2804a4fe25d3
Camilo López <camiloj.lopez79@gmail.com>
2024-07-02 02:54:03 -0400

    [FEATURE]: Implemented express validations on send email route

--- src/entities/email/routes/emailRoutes.ts
+++ src/entities/email/routes/emailRoutes.ts
@@ -1,13 +1,26 @@
 // Express
 import { Router } from 'express';
+// Express Validator
+import { check } from 'express-validator';
 // Controllers
 import { sendEmailController } from '../controllers';
 
 
 const router: Router = Router();
 
-//TODO: SendEmail
-router.post( '/contact', sendEmailController );
+// SendEmail: api/email/contact
+router.post(
+  '/contact',
+  [
+    check( 'name', 'El nombre es necesario' ).not().isEmpty(),
+    check( 'lastName', 'El apellido es necesario' ).not().isEmpty(),
+    check( 'emailAddress', 'El correo electrónico es necesario' ).isEmail(),
+    check( 'subject', 'El asunto es necesario' ).not().isEmpty(),
+    check( 'message', 'El mensaje es necesario' ).not().isEmpty()
+    // TODO: Validate fields middleware
+  ],
+  sendEmailController
+);
 
 
 
